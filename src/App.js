b97d5b151e91aaddc30ce47bb95c9edd4f1c7d71
unknown <ilyassbennane123@gmail.com>
2023-07-18 16:17:55 +0100

    hhh

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import logo from './dounia.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Dounia Rak hmara</h1>
+      <div className="image-container">
+        <img src={logo} alt="Logo" className="logo-image" />
+      </div>
     </div>
   );
 }
